9f0fc3addcbca3976f5396f5fae66a071364fdb6
Chihyun Song <ian0371@gmail.com>
2026-02-02 21:47:09 +0900

    Remove pfReport from Vrank

---
+++ blockchain/types/vrank.go
@@ -0,0 +1,52 @@
+// Copyright 2025 The Kaia Authors
+// This file is part of the Kaia library.
+//
+// The Kaia library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Kaia library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Kaia library. If not, see <http://www.gnu.org/licenses/>.
+
+package types
+
+import (
+	"github.com/kaiachain/kaia/common"
+	"github.com/kaiachain/kaia/rlp"
+)
+
+// VRankPayload is the decoded content of header.VRank (KIP-227 §4).
+// header.VRank contains only cfReport: RLP(cfReport) = RLP list of failing candidate addresses.
+// pfReport is not stored in the header; it is kept in consensus state only.
+type VRankPayload struct {
+	// CfReport is the list of candidate addresses who failed (e.g. did not send valid VRankCandidate within timeout). Nil when there are no failures.
+	CfReport []common.Address
+}
+
+// EncodeVrankPayload RLP-encodes the payload as a single list of addresses (cfReport).
+// When CfReport is nil or empty, callers may set header.VRank = nil instead of encoding.
+func EncodeVrankPayload(p *VRankPayload) ([]byte, error) {
+	if p == nil {
+		p = &VRankPayload{}
+	}
+	return rlp.EncodeToBytes(p.CfReport)
+}
+
+// DecodeVrankPayload decodes header.VRank bytes into CfReport.
+// data must be a valid RLP list of addresses.
+func DecodeVrankPayload(data []byte) (*VRankPayload, error) {
+	if len(data) == 0 {
+		return &VRankPayload{}, nil
+	}
+	var cfReport []common.Address
+	if err := rlp.DecodeBytes(data, &cfReport); err != nil {
+		return nil, err
+	}
+	return &VRankPayload{CfReport: cfReport}, nil
+}
---
+++ blockchain/types/vrank_test.go
@@ -0,0 +1,71 @@
+// Copyright 2024 The Kaia Authors
+// KIP-227 §4: header.VRank encode/decode tests.
+
+package types
+
+import (
+	"testing"
+
+	"github.com/kaiachain/kaia/common"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncodeDecodeVrankPayload_Empty(t *testing.T) {
+	// Empty payload: RLP([])
+	p := &VRankPayload{}
+	enc, err := EncodeVrankPayload(p)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, enc)
+
+	dec, err := DecodeVrankPayload(enc)
+	assert.NoError(t, err)
+	assert.NotNil(t, dec)
+	assert.Empty(t, dec.CfReport)
+}
+
+func TestEncodeDecodeVrankPayload_Nil(t *testing.T) {
+	enc, err := EncodeVrankPayload(nil)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, enc)
+
+	dec, err := DecodeVrankPayload(enc)
+	assert.NoError(t, err)
+	assert.NotNil(t, dec)
+	assert.Empty(t, dec.CfReport)
+}
+
+func TestEncodeDecodeVrankPayload_CfReportOnly(t *testing.T) {
+	// cfReport = list of candidate addresses who failed (no signatures)
+	addrs := []common.Address{
+		common.HexToAddress("0x15d34AAf54267DB7D7cC839724318F2730aC377B"),
+		common.HexToAddress("0x9965507D1a55bcC2695C58ba16FB37d819D0A4DC"),
+	}
+	p := &VRankPayload{CfReport: addrs}
+	enc, err := EncodeVrankPayload(p)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, enc)
+
+	dec, err := DecodeVrankPayload(enc)
+	assert.NoError(t, err)
+	assert.NotNil(t, dec)
+	assert.Len(t, dec.CfReport, 2)
+	assert.Equal(t, addrs[0], dec.CfReport[0])
+	assert.Equal(t, addrs[1], dec.CfReport[1])
+}
+
+func TestDecodeVrankPayload_EmptyBytes(t *testing.T) {
+	dec, err := DecodeVrankPayload(nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, dec)
+	assert.Empty(t, dec.CfReport)
+
+	dec, err = DecodeVrankPayload([]byte{})
+	assert.NoError(t, err)
+	assert.NotNil(t, dec)
+	assert.Empty(t, dec.CfReport)
+}
+
+func TestDecodeVrankPayload_InvalidRLP(t *testing.T) {
+	_, err := DecodeVrankPayload([]byte{0xff, 0xff})
+	assert.Error(t, err)
+}
